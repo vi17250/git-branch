64044a473be95624183271cc5700cd64a6b804d5
vi17250 <v.aubinaud@protonmail.com>
2025-09-04 23:00:51 +0200

    feat: 🎸 find .git directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{
+    fs::read_dir,
+    path::{Path, PathBuf},
+};
+
+const ROOT_DIR: &str = ".";
+
 fn main() {
-    println!("git_branch will be awesome 🍻");
+    let mut git_dir: PathBuf = PathBuf::from(ROOT_DIR);
+    loop {
+        if it_includes_git(git_dir.clone()) {
+            git_dir.push(".git/");
+            break;
+        } else {
+            git_dir = Path::new("../").join(git_dir);
+        }
+    }
+    println!("Git dir is: {:?}", git_dir);
+}
+
+fn it_includes_git(dir: PathBuf) -> bool {
+    let entries = read_dir(dir).expect("Failes to read dir");
+    let result = entries
+        .map(|entry| entry.expect("Failed to parse Dir name").file_name())
+        .find(|value| value == ".git");
+    match result {
+        None => false,
+        _ => true,
+    }
 }
