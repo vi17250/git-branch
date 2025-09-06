cf2f9017501d8a0b8c11ad10cda7883d237762dc
vi17250 <v.aubinaud@protonmail.com>
2025-09-06 12:39:23 +0200

    feat: 🎸 set head branch

--- src/commands/branches.rs
+++ src/commands/branches.rs
@@ -1,7 +1,7 @@
 use crate::REFS_DIR;
 use std::{
     ffi::OsString,
-    fs::read_dir,
+    fs::{read_dir, read_to_string},
     path::{Path, PathBuf},
 };
 
@@ -21,18 +21,43 @@ impl Branch {
             is_head: false,
         }
     }
+
+    fn set_is_head(&mut self, head: &OsString) {
+        if *head == self.name {
+            self.is_head = true
+        }
+    }
 }
 
 pub fn get_branches(git_dir: PathBuf) {
     let refs_dir = Path::new(&git_dir).join(REFS_DIR);
+    let head = get_head(&git_dir);
 
-    let branches = read_dir(refs_dir)
+    let mut branches = read_dir(refs_dir)
         .expect("Failes to read dir")
         .map(|entry| {
             let branch_path: PathBuf = entry.expect("Failes to parse branch path").path();
             return Branch::new(branch_path);
         })
         .collect::<Vec<Branch>>();
+
+    branches
+        .iter_mut()
+        .for_each(|mut branch| branch.set_is_head(&head));
+
     dbg!(branches);
 }
 
+fn get_head(git_dir: &PathBuf) -> OsString {
+    let head_file_path = Path::new(&git_dir).join("HEAD");
+    let content = read_to_string(head_file_path).expect("Should have been able to read the file");
+    let head_path = PathBuf::from(
+        content
+            .split(" ")
+            .last()
+            .expect("Failes to get branch")
+            .replace("\n", ""),
+    );
+    let head = head_path.file_name().expect("Failed");
+    OsString::from(head)
+}
