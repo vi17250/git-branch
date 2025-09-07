d77f26585d72e4b9d3c524bff56e903222b17854
vi17250 <v.aubinaud@protonmail.com>
2025-09-07 23:05:26 +0200

    test: 💍 branches filter

--- src/commands/dialog.rs
+++ src/commands/dialog.rs
@@ -8,8 +8,81 @@ pub fn selection(branches: Vec<Branch>) -> Vec<Branch> {
         .interact()
         .expect("Failed to read branches to remove");
 
+    filter_branches(branches, selection)
+}
+
+fn filter_branches(branches: Vec<Branch>, selection: Vec<usize>) -> Vec<Branch> {
     selection
         .iter()
         .map(|&index| branches.get(index).unwrap().clone())
         .collect::<Vec<Branch>>()
+}
+
+#[allow(warnings)]
+mod test {
+    use std::{ffi::OsString, path::PathBuf, time::SystemTime};
+
+    use crate::commands::{branches::Branch, dialog::filter_branches};
+
+    #[test]
+    fn it_returns_empty_collection() {
+        let b1 = Branch::new(
+            OsString::from("b1"),
+            PathBuf::new(),
+            PathBuf::new(),
+            false,
+            SystemTime::now(),
+        );
+        let b2 = Branch::new(
+            OsString::from("b2"),
+            PathBuf::new(),
+            PathBuf::new(),
+            false,
+            SystemTime::now(),
+        );
+        assert_eq!(filter_branches(vec![b1, b2], vec![]), vec![])
+    }
+    #[test]
+    fn it_returns_one_branche() {
+        let b1 = Branch::new(
+            OsString::from("b1"),
+            PathBuf::new(),
+            PathBuf::new(),
+            false,
+            SystemTime::now(),
+        );
+        let b2 = Branch::new(
+            OsString::from("b2"),
+            PathBuf::new(),
+            PathBuf::new(),
+            false,
+            SystemTime::now(),
+        );
+        assert_eq!(
+            filter_branches(vec![b1.clone(), b2.clone()], vec![1]),
+            vec![b2.clone()]
+        )
+    }
+
+    #[test]
+    fn it_returns_all_branches() {
+        let b1 = Branch::new(
+            OsString::from("b1"),
+            PathBuf::new(),
+            PathBuf::new(),
+            false,
+            SystemTime::now(),
+        );
+        let b2 = Branch::new(
+            OsString::from("b2"),
+            PathBuf::new(),
+            PathBuf::new(),
+            false,
+            SystemTime::now(),
+        );
+        assert_eq!(
+            filter_branches(vec![b1.clone(), b2.clone()], vec![0, 1]),
+            vec![b1.clone(), b2.clone()]
+        )
+    }
 }
