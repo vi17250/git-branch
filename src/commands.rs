384cbaa1197d83c44d891deccb715c9e4bb50f98
vi17250 <v.aubinaud@protonmail.com>
2025-09-07 18:24:39 +0200

    feat: 🎸 add last update to Branch struct

--- src/commands.rs
+++ src/commands.rs
@@ -1,1 +1,2 @@
 pub mod branches;
+pub mod dialog;
--- src/commands/branches.rs
+++ src/commands/branches.rs
@@ -1,8 +1,10 @@
 use crate::{HEAD, LOGS_DIR, REFS_DIR};
 use std::{
     ffi::OsString,
+    fmt::{Display, Formatter, Result},
     fs::{read_dir, read_to_string},
     path::{Path, PathBuf},
+    time::SystemTime,
 };
 
 #[derive(Debug)]
@@ -11,42 +13,70 @@ pub struct Branch {
     refs_dir: PathBuf,
     logs_dir: PathBuf,
     is_head: bool,
+    last_update: SystemTime,
 }
 
 impl Branch {
-    fn new(name: OsString, refs_dir: PathBuf, logs_dir: PathBuf, is_head: bool) -> Branch {
+    fn new(
+        name: OsString,
+        refs_dir: PathBuf,
+        logs_dir: PathBuf,
+        is_head: bool,
+        last_update: SystemTime,
+    ) -> Branch {
         Branch {
             name,
             refs_dir,
             logs_dir,
             is_head,
+            last_update,
         }
+    }
+
+    pub fn is_removable(&self) -> bool {
+        !self.is_head
     }
 }
 
-pub fn get_branches(git_dir: PathBuf) {
+impl Display for Branch {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let name = self
+            .name
+            .to_str()
+            .expect("Failed to parse OsString to String");
+
+        write!(f, "{}\t", name,)
+    }
+}
+
+pub fn get_branches(git_dir: PathBuf) -> Vec<Branch> {
     let refs_dir = Path::new(&git_dir).join(REFS_DIR);
     let logs_dir = Path::new(&git_dir).join(LOGS_DIR);
     let head = get_head(&git_dir);
 
     let branches = read_dir(&refs_dir)
         .expect("Failes to read dir")
-        .map(|entry| {
-            let branch_name = entry
-                .expect("Failes to parse branch path")
-                .path()
-                .file_name()
-                .expect("Failed to read branch name")
-                .to_os_string();
-            return Branch::new(
-                branch_name.clone(),
-                PathBuf::from(&refs_dir).join(&branch_name),
-                PathBuf::from(&logs_dir).join(&branch_name),
-                branch_name == head,
-            );
+        .map(|entry| match entry {
+            Ok(entry) => {
+                let time = entry
+                    .metadata()
+                    .expect("Failed to parse metadata")
+                    .modified()
+                    .expect("Failed to parse system time");
+
+                let branch_name = entry.path().file_name().expect("WTF").to_os_string();
+                return Branch::new(
+                    branch_name.clone(),
+                    PathBuf::from(&refs_dir).join(&branch_name),
+                    PathBuf::from(&logs_dir).join(&branch_name),
+                    *branch_name == head,
+                    time,
+                );
+            }
+            Err(_) => panic!("Failed to parse entry"),
         })
         .collect::<Vec<Branch>>();
-    dbg!(branches);
+    branches
 }
 
 fn get_head(git_dir: &PathBuf) -> OsString {
