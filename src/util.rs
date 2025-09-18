daa5bc2ef0285884f28bba93f733ef787e943d70
vi17250 <v.aubinaud@protonmail.com>
2025-09-19 00:14:19 +0200

    fix: 🐛 displays the human-readable modification time

---
+++ src/util.rs
@@ -0,0 +1,71 @@
+use std::fmt::format;
+
+pub fn parse_time(seconds: &u64) -> String {
+    match seconds {
+        seconds if *seconds < 60 => format!("{seconds} sec"),
+        seconds if *seconds < 7200 => {
+            let min = seconds / 60;
+            format!("{min} min")
+        }
+        seconds if *seconds < 86400 => {
+            let hours = seconds / 3600;
+            format!("{hours} hour(s)")
+        }
+        seconds if *seconds < 604800 => {
+            let days = seconds / 86400;
+            format!("{days} day(s)")
+        }
+        seconds if *seconds < 2592000 => {
+            let weeks = seconds / 604800;
+            format!("{weeks} week(s)")
+        }
+        _ => {
+            let month = seconds / 2592000;
+            format!("{month} month(s)")
+        }
+    }
+}
+
+#[allow(warnings)]
+mod test {
+    use crate::util::parse_time;
+
+    #[test]
+    fn it_returns_duration_in_seconds() {
+        assert_eq!(parse_time(&10), "10 sec")
+    }
+
+    #[test]
+    fn it_returns_duration_in_minutes() {
+        assert_eq!(parse_time(&61), "1 min");
+        assert_eq!(parse_time(&301), "5 min");
+        assert_eq!(parse_time(&360), "6 min");
+        assert_eq!(parse_time(&3600), "60 min");
+        assert_eq!(parse_time(&5460), "91 min");
+    }
+
+    #[test]
+    fn it_returns_duration_in_hours() {
+        assert_eq!(parse_time(&10800), "3 hour(s)");
+        assert_eq!(parse_time(&43200), "12 hour(s)");
+    }
+
+    #[test]
+    fn it_returns_duration_in_days() {
+        assert_eq!(parse_time(&86400), "1 day(s)");
+        assert_eq!(parse_time(&433000), "5 day(s)");
+    }
+
+    #[test]
+    fn it_returns_duration_in_weeks() {
+        assert_eq!(parse_time(&691200), "1 week(s)");
+        assert_eq!(parse_time(&1815400), "3 week(s)");
+    }
+
+    #[test]
+    fn it_returns_duration_in_month() {
+        assert_eq!(parse_time(&2592000), "1 month(s)");
+        assert_eq!(parse_time(&7776000), "3 month(s)");
+        assert_eq!(parse_time(&15555000), "6 month(s)");
+    }
+}
