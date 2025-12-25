b9ea1686c1c6801cb036eadda35347388aac2f7c
vi17250 <v.aubinaud@protonmail.com>
2025-12-25 19:28:29 +0100

    fix: 🐛 Throw an error and quit if there are no existing commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::{env, fs::read_dir, path::PathBuf};
 use console::style;
+use std::{
+    env,
+    fs::read_dir,
+    path::{Path, PathBuf},
+};
 use valinta::select;
 
 mod branches;
-use branches::utils::{delete_branches, get_branches};
 use branches::def::Branch;
-
+use branches::utils::{delete_branches, get_branches};
 
 mod dialog;
 use dialog::confirm::confirm;
@@ -14,6 +17,7 @@ mod util;
 mod error;
 pub use crate::error::{Error, Result};
 
+const COMMIT_EDITMSG: &str = "COMMIT_EDITMSG";
 const REFS_DIR: &str = "refs/heads";
 const LOGS_DIR: &str = "logs/refs/heads";
 const HEAD: &str = "HEAD";
@@ -33,6 +37,12 @@ fn main() -> Result<()> {
             );
         }
     }
+
+    let commit_exist: bool = Path::new(&current_dir).join(COMMIT_EDITMSG).exists();
+    if !commit_exist {
+        return Err("It appears that you have not yet created any commits".into());
+    }
+
     let mut branches: Vec<Branch> = get_branches(&current_dir)?;
 
     let origin_branch = branches.iter().find(|branch| branch.is_origin());
@@ -47,26 +57,24 @@ fn main() -> Result<()> {
         None => "origin is empty on this repository".to_string(),
     };
 
-    let head_branch_index = branches
-        .iter()
-        .position(|branch| branch.is_head())
-        .ok_or("It appears that you have not yet created any commits.")?;
+    let head_branch = branches.iter().position(|branch| branch.is_head()).map(|index| branches.remove(index));
 
-    let head_branch = branches.remove(head_branch_index);
+    let head_message: String = match head_branch {
+        Some(branch) => format!(
+            "{} {} {}",
+            style("HEAD").color256(6).bold(),
+            style("->").color256(202),
+            branch
+        ),
+        None => String::new(),
+    };
 
     if branches.is_empty() {
-        println!("No banches to delete");
-        return Ok(());
+        return Err("No branches to delete".into());
     }
-    let head = format!(
-        "{} {} {}",
-        style("HEAD").color256(6).bold(),
-        style("->").color256(202),
-        head_branch
-    );
 
     let intro = style("Which branches do you want to delete?").bold();
-    println!("{origin}\n{head}\n\n{intro}");
+    println!("{origin}\n{head_message}\n\n{intro}");
 
     let mut branches_to_delete = select(&branches)?.0;
 
