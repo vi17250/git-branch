5b2e1c62666991de078ea656cc315a98f325e102
vi17250 <v.aubinaud@protonmail.com>
2026-04-02 00:48:26 +0200

    refactor: 💡 GIT_DIR is static and available in all the modules

--- src/branches/head.rs
+++ src/branches/head.rs
@@ -4,7 +4,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use crate::HEAD;
+use crate::{GIT_DIR, HEAD};
 
 #[derive(Debug)]
 pub struct Head {
@@ -16,7 +16,8 @@ impl Head {
     ///
     /// It reads the content if the .git/HEAD file and parses it.
     /// The result of the parsing (the filename) is the name of the current branch
-    pub fn init(git_dir: &PathBuf) -> Result<Self> {
+    pub fn init() -> Result<Self> {
+        let git_dir = GIT_DIR.get().context("Failed to retrieve .git directory")?;
         let head_file_path: PathBuf = Path::new(&git_dir).join(HEAD);
         let content = read_to_string(head_file_path).context("Failed to read `.git/HEAD` file")?;
         let path = Path::new(&content);
